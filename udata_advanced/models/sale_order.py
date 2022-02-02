36db86f6b7fe743f17d8f9e4caecc3c5a0780243
khalil <khalil.elsherif@crevisoft.com>
2022-02-02 21:28:44 +0200

    update ksa e invoice

--- udata_advanced/models/sale_order.py
+++ udata_advanced/models/sale_order.py
@@ -10,6 +10,17 @@ class SaleOrder(models.Model):
 
     sale_terms_and_conditions = fields.Text(string='Terms and Conditions')
     arabic_sale_terms_and_conditions = fields.Text(string='Terms and Conditions')
+    total_amount_discount = fields.Float(string="Amount Discount", store=True, compute="_compute_amount_discount",
+                                   readonly=True)
+
+
+    @api.depends("order_line")
+    def _compute_amount_discount(self):
+        for order in self:
+            order.total_amount_discount = round(sum(line.amount_discount for line in order.order_line), 2)
+
+
+
 
 
     @api.model
@@ -34,5 +45,21 @@ class SaleOrder(models.Model):
 
     def _get_url_order(self):
         return 'https://udata22.odoo.com' + self.get_portal_url()
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+
+    amount_discount = fields.Float(string="Amount Discount", store=True, compute="_compute_discount",
+                                   readonly=True)
+
+
+    @api.depends("price_unit", "discount", "product_uom_qty")
+    def _compute_discount(self):
+        for line in self:
+            line.amount_discount = line.discount != 0 and (line.price_unit * line.discount / 100) * line.product_uom_qty or 0
+
+
 
 
